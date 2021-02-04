2d35308abc719f8084667d09423164fff26120eb
Drishtant Regmi <drishtantregmi777@gmail.com>
2021-02-04 21:40:36 +0545

    Day 6

---
+++ Day 6/minDist.js
@@ -0,0 +1,39 @@
+const blocks = [
+    {
+        'gym': false,
+        'school': true,
+        'store': false 
+    },
+    {
+        'gym': true,
+        'school': false,
+        'store': false 
+    },
+    {
+        'gym': true,
+        'school': true,
+        'store': false
+    },
+    {
+        'gym': false,
+        'school': true,
+        'store': false
+    },
+    {
+        'gym': false,
+        'school': true,
+        'store': true
+    },
+]
+
+var number = 0;
+// let min = blocks[0].filter(word => word = true)
+// for (let i=0; i<blocks[0]; i++) {
+    
+// }
+
+for (let i=0; i<blocks.length; i++) {
+    number++
+}
+
+console.log(number);
