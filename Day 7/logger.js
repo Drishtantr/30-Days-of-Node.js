ea5cc0b461ab08afe04ef8ff64ff62bdb9aba035
Drishtant Regmi <drishtantregmi777@gmail.com>
2021-02-05 21:11:30 +0545

    Day 7

---
+++ Day 7/logger.js
@@ -0,0 +1,9 @@
+var url = 'http://mylogger.io/log';
+
+function log(message) {
+    // Send an HTTP request
+    console.log(message+ '  ' + url);
+}
+
+module.exports.log = log;
+// module.exports.endPoint = url;
