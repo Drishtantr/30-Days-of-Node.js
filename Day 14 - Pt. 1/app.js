910384dd50efe3a42b91d5e9f642f87067bfcd5e
Drishtant Regmi <drishtantregmi777@gmail.com>
2021-02-16 15:26:10 +0545

    Day 14 - FS Module

---
+++ Day 14 - Pt. 1/app.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+
+const files = fs.readdirSync('./');
+console.log(files);
+
+fs.readdir('./', (err, files) => {
+    if (err) {
+        console.log(`Error: ${err}`);
+    }
+    else {
+        console.log(`Result: ${files}`);
+    }
+});
