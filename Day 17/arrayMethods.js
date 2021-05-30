2cd4ae04b0f00f0f9051dde78cba720d331b94fe
Drishtant Regmi <drishtantregmi777@gmail.com>
2021-05-30 21:33:17 +0545

    Day 17- Array Methods

---
+++ Day 17/arrayMethods.js
@@ -0,0 +1,53 @@
+const items = [
+  { name: "Apple", price: 10 },
+  { name: "Ball", price: 20 },
+  { name: "Cat", price: 30 },
+  { name: "Dog", price: 40 },
+  { name: "Eagle", price: 50 },
+];
+
+// Filter
+const filarr = items.filter((item) => {
+  return item.price < 30;
+});
+console.log(filarr);
+
+// Map
+const maparr = items.map((item) => {
+  return item.name;
+});
+console.log(maparr);
+
+// Find
+const findarr = items.find((item) => {
+  return item.name === "Dog";
+});
+console.log(findarr);
+
+// foreach
+items.forEach((item) => {
+  console.log(item.name);
+});
+
+// Some
+const somearr = items.some((item) => {
+  return item.price > 15;
+});
+console.log(somearr);
+
+// every
+const everyarr = items.every((item) => {
+  return item.price > 9;
+});
+console.log(everyarr);
+
+// reduce
+const reducearr = items.reduce((currentTotal, item) => {
+  return item.price + currentTotal;
+}, 0);
+console.log(reducearr);
+
+//includes
+const listt = [1, 2, 3, 4, 5];
+const incl = listt.includes(5);
+console.log(incl);
